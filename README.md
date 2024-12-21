c92ad96d8e4b822a5c792be7ae2cdf94ffa875fb
d-zhuye <180813277+d-zhuye@users.noreply.github.com>
2024-12-21 14:57:15 -0800

    Add basic webpack confirgutations

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # webpack-common-starter-template
+
+Separate development and production builds per instructions from https://webpack.js.org/guides/production/
+
+
---
+++ webpack.common.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+  entry: {
+    app: "./src/index.js",
+  },
+  output: {
+    filename: "[name].bundle.js",   // Change filename
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        }
+    ]
+  }
+};
